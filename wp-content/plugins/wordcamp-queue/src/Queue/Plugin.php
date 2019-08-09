287c6889cbb935c0dc59deb1f1fc4fcc553da907
Jonathan Brinley <jbrinley@flightless.us>
2019-08-08 22:25:03 -0400

    register the database tables

--- wp-content/plugins/wordcamp-queue/src/Queue/Plugin.php
+++ wp-content/plugins/wordcamp-queue/src/Queue/Plugin.php
@@ -36,6 +36,7 @@ class Plugin {
 	}
 
 	private function load_service_providers() {
+		$this->providers['schema'] = new Container\Schema_Provider();
 
 		/**
 		 * Filter the service providers the power the plugin
